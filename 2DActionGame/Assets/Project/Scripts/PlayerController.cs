eb05b9eb4e3844260d65ee5312514d5619e09695
hizuki sakai <mosdocomo@gmail.com>
2019-04-27 02:49:56 +0900

    AddScript

---
+++ 2DActionGame/Assets/Project/Scripts/PlayerController.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TouchSystemManager;
+using UnityEngine;
+
+public abstract class PlayerController :MonoBehaviour
+{
+    [Header("[Setting]")]
+    public float MoveSpeed = 6;
+    public int JumpCount = 2;
+    public float jumpForce = 15f;
+    public Vector2 speedLv;     // 移動速度
+    public Vector2 jumpLv;      // ジャンプ力
+
+    TouchManager touchManager;
+    TouchSystemManagerProperty[] touchPropertys;
+    public Animator anime_Swordman;
+
+    public bool isSit;
+    public bool isAttack;
+    public bool isMove;
+    public bool isMoveRight;
+    public bool isMoveLeft;
+    public bool isJump;
+    public bool isGrounded;
+    public bool isOnceJump;
+    public bool isDownJump;   // 落下或いは着地の判定結果
+    public bool isDead;
+
+
+    public int currentJumpCount = 0;
+
+    protected float m_MoveX;  // なんやろこいつ消したい
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedLv = new Vector2(2.0f, 0.0f);  // コンポーネントに代入 
+        jumpLv = new Vector2(300.0f, 0.0f); // addforceで飛ぶ
+
+        touchManager = new TouchManager();
+        touchPropertys = touchManager.getTouchManager();
+
+        isSit = false;              // 座っている
+        isAttack = false;           // 攻撃している
+        isMove = false;             // 移動している
+        isJump = false;             // ジャンプしている
+        isGrounded = false;         // 地に足がついてる
+        isOnceJump = false;         // 2段ジャンプができる
+        isDownJump = false;         // 落下或いは着地の判定結果
+        StartPlus();
+    }
+
+    public abstract void StartPlus();
+
+    //********************************************************************************************
+    //********************************************************************************************
+
+    void Update()
+    {
+        touchManager.TouchUpdate();
+        touchPropertys = touchManager.getTouchManager();
+
+        for (var i = 0; i < touchManager.getMaxTouches(); i++)
+        {
+            if (touchPropertys[i].touchFlag)
+            {
+                if (touchPropertys[i].nowTouchPhase == TouchPhase.Began)
+                {
+
+                }
+                else if (touchPropertys[i].nowTouchPhase == TouchPhase.Moved)
+                {
+                    isMove = true;
+                }
+                else if (touchPropertys[i].nowTouchPhase == TouchPhase.Ended)
+                {
+
+                }
+            }
+        }
+        UpdatePlus();
+    }
+
+    public abstract void UpdatePlus();
+
+    private void FixedUpdate()
+    {
+        for (var i = 0; i < touchManager.getMaxTouches(); i++)
+        {
+            if (touchPropertys[i].nowTouchPhase == TouchPhase.Began && isMove)
+            {
+            }
+            else if (touchPropertys[i].nowTouchPhase == TouchPhase.Moved && isMove)
+            {
+                // 移動ベクトル方向取得
+                Vector2 vct = (touchPropertys[i].endTouchPosition - touchPropertys[i].startTouchPosition).normalized;
+                float rad = Mathf.Rad2Deg * (Mathf.Atan2(vct.x, vct.y));    // 入力したベクトルを弧度法radとして再取得
+                if (rad <= 85.0f || rad >= 275.0f)
+                {// X軸正移動
+                }
+                if (rad >= 95.0f && rad <= 265.0f)
+                {// X軸負移動
+                }
+
+                // Y軸判定
+                if (rad > 80.0f && rad < 100.0f)
+                {// 上入力判定（ジャンプ）
+                    this.gameObject.GetComponent<Rigidbody2D>().AddForce(jumpLv);
+                }
+                else if (rad > 260.0f && rad < 280.0f)
+                {// 下入力判定
+
+                }// 
+
+
+
+
+                this.gameObject.GetComponent<Rigidbody2D>().velocity = vct * 2.0f;
+
+                Debug.Log("マウス長押し　vecter：" + vct);
+            }
+            else if (touchPropertys[i].nowTouchPhase == TouchPhase.Ended && isMove)
+            {
+                this.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                isMove = false;
+            }
+        }
+    }
+
+    //********************************************************************************************
+    //********************************************************************************************
+
+
+
+
+
+    protected void prefromJump()
+    {// わからん
+        anime_Swordman.Play("Jump");
+
+        this.transform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        this.transform.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        isOnceJump = true;
+        isGrounded = false;
+
+
+        currentJumpCount++;
+
+    }
+
+    protected void DownJump()
+    {// わからん
+        if (!isGrounded)
+            return;
+
+
+        if (!isDownJump)
+        {
+            anime_Swordman.Play("Jump");
+
+            this.transform.GetComponent<Rigidbody2D>().AddForce(-Vector2.up * 10);
+            isGrounded = false;
+
+            this.transform.GetComponent<CapsuleCollider2D>().enabled = false;
+
+            StartCoroutine(GroundCapsulleColliderTimmerFuc());
+
+        }
+    }
+
+    IEnumerator GroundCapsulleColliderTimmerFuc()
+    {// わからん
+        yield return new WaitForSeconds(0.3f);
+        this.transform.GetComponent<CapsuleCollider2D>().enabled = true;
+    }
+
+
+    //////着地判定 
+    // わからん
+    Vector2 RayDir = Vector2.down;
+
+
+    float PretmpY;
+    float GroundCheckUpdateTic = 0;
+    float GroundCheckUpdateTime = 0.01f;
+    protected void GroundCheckUpdate()
+    {
+        if (!isOnceJump)
+            return;
+
+        GroundCheckUpdateTic += Time.deltaTime;
+
+        if (GroundCheckUpdateTic > GroundCheckUpdateTime)
+        {
+            GroundCheckUpdateTic = 0;
+
+            if (PretmpY == 0)
+            {
+                PretmpY = transform.position.y;
+                return;
+            }
+
+            float reY = transform.position.y - PretmpY;  //    -1  - 0 = -1 ,  -2 -   -1 = -3
+
+            if (reY <= 0)
+            {
+                if (isGrounded)
+                {
+                    LandingEvent();
+                    isOnceJump = false;
+
+                }
+                else
+                {
+
+                }
+            }
+            PretmpY = transform.position.y;
+
+        }
+    }
+    protected abstract void LandingEvent();
+
+}
---
+++ 2DActionGame/Assets/Project/Scripts/Swordman_AnimationLogic.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swordman_AnimationLogic : PlayerController
+{
+    protected void AnimUpdate()
+    {// 親クラスのメンバや現在のアニメーション動作情報を参照し、
+     // 次のアニメーション動作を決定する
+        if (!anime_Swordman.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {// アニメーションステートが"Attack"でない
+
+            if (isAttack)
+            {// 攻撃ボタンを押した
+
+                anime_Swordman.Play("Attack");
+            }
+            else
+            {// 攻撃ボタンを押してない
+
+                if (isMove)
+                {// 移動してる
+
+                    anime_Swordman.Play("Run");
+                }
+                else
+                {// 移動してない
+
+                    if (!isJump) // ジャンプしてない
+                        anime_Swordman.Play("Idle");
+
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+
+        checkInput();
+
+        if (this.transform.GetComponent<Rigidbody2D>().velocity.magnitude > 30)
+        {
+            this.transform.GetComponent<Rigidbody2D>().velocity =
+                new Vector2(this.transform.GetComponent<Rigidbody2D>().velocity.x - 0.1f, this.transform.GetComponent<Rigidbody2D>().velocity.y - 0.1f);
+
+        }
+    }
+
+    public void checkInput()
+    {// 各入力チェックから動作を決定する
+
+        if (isSit)
+        {// 下入力あり
+            anime_Swordman.Play("Sit");
+        }
+        else
+        {// 下入力なし
+
+            anime_Swordman.Play("Idle");
+            isSit = false;
+        }
+
+        if (anime_Swordman.GetCurrentAnimatorStateInfo(0).IsName("Sit") || anime_Swordman.GetCurrentAnimatorStateInfo(0).IsName("Die"))
+        {// 良くない記述な気がする
+            if (isSit)
+            {
+                if (currentJumpCount < JumpCount)  // 0 , 1
+                {
+                    DownJump();
+                }
+            }
+
+            return;
+        }
+
+        GroundCheckUpdate();
+
+        if (!anime_Swordman.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            if (isAttack)
+            {
+
+                anime_Swordman.Play("Attack");
+            }
+            else
+            {
+
+                if (isMove)
+                {
+                    if (!isOnceJump)
+                        anime_Swordman.Play("Idle");
+
+                }
+                else
+                {
+
+                    anime_Swordman.Play("Run");
+                }
+            }
+        }
+
+        if (isDead)
+        {
+            anime_Swordman.Play("Die");
+        }
+
+        // 기타 이동 인풋.
+
+
+        if (!anime_Swordman.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {// 攻撃中でない時、移動の向きを補正
+            if (isMoveLeft)
+                if (!isMoveRight)
+                    Filp(false);
+                else return;
+            else if (isMoveRight)
+                if (!isMoveLeft)
+                    Filp(true);
+        }
+
+
+        if (isJump)
+        {// 攻撃中でなければジャンプモーション
+            if (anime_Swordman.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+                return;
+            if (currentJumpCount < JumpCount)  // 0 , 1
+            {
+                if (!isSit)
+                {
+                    prefromJump();
+                }
+                else
+                {
+                    DownJump();
+                }
+            }
+        }
+    }
+
+    protected void Filp(bool bLeft)
+    {// キャラ向き反転
+
+        transform.localScale = new Vector3(bLeft ? 1 : -1, 1, 1);
+    }
+
+    protected override void LandingEvent()
+    {// わからん
+
+        if (!anime_Swordman.GetCurrentAnimatorStateInfo(0).IsName("Run") && !anime_Swordman.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+            anime_Swordman.Play("Idle");
+    }
+    public override void StartPlus()
+    {// start処理に追加したい場合はここに記述
+        // そもそもこの記述をしなくてもオーバーライドしないので不要
+    }
+    public override void UpdatePlus()
+    {// update処理に追加したい場合はここに記述
+        // そもそもこの記述をしなくてもオーバーライドしないので不要
+    }
+}
